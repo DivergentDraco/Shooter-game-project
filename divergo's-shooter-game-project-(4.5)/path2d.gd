75a60382bc25f01ee29b96d6ee44931d8fcf1e33
DivergentDraco <ryukrub@gmail.com>
2026-09-05 17:25:54 +0700

    Deleted old system variant

--- divergo's-shooter-game-project-(4.5)/path2d.gd
+++
@@ -1,9 +0,0 @@
-extends Path2D
-
-var speed = 100
-
-@onready var screensize  = get_viewport_rect().size
-@onready var pathfollow:PathFollow2D = $PathFollow2D
-	
-func _process(delta: float) -> void:
-	pathfollow.progress += speed * delta
